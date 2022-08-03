e30b1fa43f6f2f8521e23e525673372758d4de77
Mávery Vargas <maveryvargas@gmail.com>
2022-08-03 11:22:50 -0300

    Adicionar arquivos de projeto.

---
+++ ExperimentoComClasses/House.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentoComClasses
+{
+    class House
+    {
+        Boolean prop;
+        private string nomeDono;
+
+        public void Dono()
+        {
+            nomeDono = "Mavery";
+            string EndCasa = "Rua XPTO, bairro TESTE, num 000";
+
+            Console.WriteLine("Voce e o dono da casa? (Digite 1 para verdadeiro ou 0 para falso)");
+            prop =Convert.ToBoolean(Console.ReadLine());
+            if(prop == true)
+            {
+                Console.WriteLine("Qual é seu nome?");
+                Console.WriteLine("Voce de nome "+nomeDono+" é proprietario da casa no endereço"+EndCasa);
+            }
+            else
+            {
+                Console.WriteLine("Você nao e o dono da casa");
+            }
+        }
+
+        public bool VerificaDono()
+        {
+            string nomeDono = "Mavery";
+            Console.WriteLine("Digite o seu nome");
+            var aux = Convert.ToString(Console.ReadLine());
+            if (aux == nomeDono)
+            {
+                Console.WriteLine("Você pode entrar");
+                return true;
+            }
+            else Console.WriteLine("Voce nao é o dono, nao pode entrar");
+            return false;
+        }
+        public void Acende()
+        {
+            Console.WriteLine("As luzes estao acesas");
+        }
+        public void Apagar()
+        {
+            Console.WriteLine("As luzes estao apagadas");
+        }
+    }
+}
---
+++ ExperimentoComClasses/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExperimentoComClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            House casa1 = new House();
+            House casa2 = new House();
+
+
+            casa1.VerificaDono();
+            //casa1.VerificaDono();
+            //casa1.Acende();
+            //casa2.Apagar();
+        }
+    }
+}
